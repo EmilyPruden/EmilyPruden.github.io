db4512ce26aff82ca34461ca7aacb59b75d4b7f2
EmilyPruden <a02328112@usu.edu>
2025-11-09 23:55:44 -0700

    Homework 9 changes

--- javajam/js/verifysenior.js
+++ javajam/js/verifysenior.js
@@ -3,8 +3,8 @@ var age;
 age = prompt("Enter your age:")
 if (age >= 65) {
     output = "Free Friday Coffee Night for Seniors!" 
-    document.getElementById('verify').style.color = 'maroon';
-    document.getElementById('verify').style.fontVariantCaps = 'small-caps';
+    document.getElementById('verify').style.color = '#231814';
+    document.getElementById('verify').style.fontVariantCaps = 'medium-caps';
 } else {
     output = "Enjoy Music and Make Memories!"
 }
